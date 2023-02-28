052698d56e53459c1ac045f66eec6c8568f0203f
Abdul Wahid Lak <mailtoa.wahid@gmail.com>
2023-02-28 13:05:34 +0400

    fixed

--- src/pages/Transection/Transection.js
+++ src/pages/Transection/Transection.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
 import BottomMenu from "../../components/BottomMenu";
+import axios from 'axios';
 import { Chip } from '@mui/material';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import { APIGetAllTransactions } from '../../helpers/APIs/TransactionAPI';
 import CircularProgress from '@mui/material/CircularProgress';
-
 
 const Transection = () => {
   const [transections, setTransections] = useState([]);
@@ -32,9 +32,6 @@ const Transection = () => {
     setLoading(false);
   }
 
-
-
-
   const handleChange = (e, newValue) => {
     setActiveTab(newValue);
     if (newValue === "1") {
@@ -43,8 +40,6 @@ const Transection = () => {
       setSelectedTransactions(transections.filter(t => t.transaction_purpose === "withdraw"));
     }
   }
-
-
 
   return (
     <div style={{ display: 'flex', height: '100vh', flexDirection: 'column', gap: 15 }}>
@@ -65,12 +60,12 @@ const Transection = () => {
                   <th style={{ paddingBottom: '10px' }}>Trạng thái</th>
                 </tr>
               </thead>
-              {transections && transections.map((transection) => {
+              {selectedTransactions && selectedTransactions.map((transection) => {
                 return (
-                  <tbody key={transection.id}>
-                    <tr>
+                  <tbody>
+                    <tr key={transection.id}>
                       <td>{transection.transaction_purpose == 'deposit' ? '+' : '-'}{transection.transaction_amount}</td>
-                      <td>{new Date(transection.created_at).toLocaleDateString("vi-VN")}</td>
+                      <td >{new Date(transection.created_at).toLocaleString("vi-VN")}</td>
                       <td><Chip label={transection.is_approved === 0 ? 'Đang xử lý' : transection.is_approved === 1 ? 'Đã phê duyệt' : 'Từ chối'}
                         sx={{ backgroundColor: transection.is_approved === 0 ? '#53a9a3' : transection.is_approved === 1 ? 'green' : '#ad2626', color: 'white' }} /></td>
                     </tr>
